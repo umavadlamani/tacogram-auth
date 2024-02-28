a379b631d5534598374bfebb1af276139d5eb447
umavadlamani <155587825+umavadlamani@users.noreply.github.com>
2024-02-28 08:01:50 -0600

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new
+    @user["first_name"] = params["first_name"]
+    @user["last_name"] = params["last_name"]
+    @user["email"] = params["email"]
+    # TODO: encrypt user's password "at rest"
+    @user["password"] = params["password"]
+    @user.save
+    redirect_to "/posts"
+  end
+end
